e729a39ddf247435e459388d44202e54dc62066b
zywang <470810634@qq.com>
2020-02-24 10:57:40 +0800

    add the transmit Augment function

--- utils/augment.py
+++ utils/augment.py
@@ -2,12 +2,63 @@ import pandas as pd
 import jieba
 import random
 import os
-
+import numpy as np
 
 class DataAugment:
     def __init__(self):
         self.columns = ['question1', 'question2', 'label', 'category']
         self.save_path = '../try_data/augment.csv'
+
+    def sentence_set_pair(self, df_train, num=None):
+        questions1 = []
+        questions2 = []
+        labels = []
+        categories = []
+        column1, column2, column3, column4 = self.columns[0], self.columns[1], self.columns[2], self.columns[3]
+        query_1_list = list(set(df_train['question1']))
+        for query_tag in query_1_list:
+            df_query = df_train[df_train['question1']==query_tag]
+            query_same_set = df_query[df_query['label']==1]['question2'].tolist()
+            query_diff_set = df_query[df_query['label']==0]['question2'].tolist()
+            category = list(df_query['category'])[0]
+            if len(query_same_set) >= 1:   #如果有与query1相似的问题
+                if len(query_diff_set) >= 1:   #类别间
+                    for query_1 in query_same_set:
+                        for query_2 in query_diff_set:
+                            questions1.append(query_1)
+                            questions2.append(query_2)
+                            labels.append('0')
+                            categories.append(category)
+                if len(query_same_set) >= 2:   #类别内
+                    for i in range(len(query_same_set)-1):
+                        for j in range(i+1, len(query_same_set)):
+                            questions1.append(query_same_set[i])
+                            questions2.append(query_same_set[j])
+                            labels.append('1')
+                            categories.append(category)
+        new_df = pd.DataFrame(
+            {column1: questions1, column2: questions2, column3: labels, column4: categories})
+        positive_num = len(new_df[new_df[column3]=='1'])
+        negative_num = len(new_df[new_df[column3]=='0'])
+
+        if num == None:
+            print("postive num in all augment data: {:.2f}%".format(positive_num/(positive_num + negative_num)*100))
+            return new_df
+        else:
+            if min(positive_num, negative_num) >= num/2:
+                extract_num = int(num/2)
+            else:
+                extract_num = int(min(positive_num, negative_num))
+            df_postive = new_df[new_df[column3] == '1']
+            df_negative = new_df[new_df[column3] == '0']
+            df_postive = df_postive.sample(n=extract_num, replace=False)
+            df_negative = df_negative.sample(n=extract_num, replace=False)
+            new_df = pd.concat([df_postive, df_negative], ignore_index=True)
+            new_df = new_df.sample(frac=1.0, replace=False)
+            print("choose the balanced augment data")
+            return new_df
+
+
 
     def sentnteces_dropsameword(self, df_train, prob):
         def compute_same_begainer(q1, q2):
@@ -107,22 +158,22 @@ class DataAugment:
         for row in datalist:
             question1.append(row[0])
             question2.append(row[1])
-            label.append(row[2])
+            label.append(int(row[2]))
             category.append(row[3])
         return pd.DataFrame({'question1':question1, 'question2':question2, 'label':label, 'category':category})
 
     def dataframe_list(self, dataframe):
         data_list = []
         for idx, row in dataframe.iterrows():
-            row_list = [row['question1'], row['question2'], row['label'], row['category']]
+            row_list = [row['question1'], row['question2'], str(row['label']), row['category']]
             data_list.append(row_list)
         return data_list
 
     def dataAugment(self, train_list, *args):
         #args:'symmetric'=> symmetric_sentence, 'themword'=>sentences_dropthemword, 'pseudo'=>Pseudolabel
         for arg in args:
-            if arg not in ['symmetric', 'themword', 'pseudo', 'sameword']:
-                raise ValueError('the input must choose from [''symmetric', 'themword', 'pseudo'']')
+            if arg not in ['symmetric', 'themword', 'pseudo', 'sameword', 'transmit']:
+                raise ValueError('the input must choose from [''symmetric', 'themword', 'pseudo', 'transmit'']')
         keywords = ['糖尿病', '高血压', '艾滋病', '乳腺癌', '乙肝']
         prob = True
         df_train = self.list_dataframe(train_list)
@@ -135,6 +186,8 @@ class DataAugment:
                 new_data = self.sentnteces_dropsameword(df_train, prob)
             elif arg == 'pseudo':
                 new_data = self.Pseudolabel()
+            elif arg == 'transmit':
+                new_data = self.sentence_set_pair(df_train, num=3000)
             else:
                 raise ValueError("The input must choose from ['symmetric','themword','pseudo', 'sameword']")
             if idx == 0:
@@ -144,13 +197,4 @@ class DataAugment:
 
         # print('genarate data finished')
         return self.dataframe_list(all_data)
-
-
-#test
-#if __name__ == "__main__":
-#    try_data = [['艾滋病窗口期会出现腹泻症状吗', '艾滋病窗口期会出现艾滋病头疼腹泻四肢无力是不是', 0, 'aids'], ['由于引起末梢神经炎，怎么根治？', '末梢神经炎的治疗方法', 1, 'diabetes'],['艾滋病窗口期会出现腹泻症状吗', '艾滋病窗口期会出现艾滋病头疼腹泻四肢无力是不是', 0, 'aids'], ['艾滋病窗口期会出现腹泻症状吗', '艾滋病窗口期会出现艾滋病头疼腹泻四肢无力是不是', 0, 'aids']]
-#    Augment = DataAugment()
-#    data = Augment.dataAugment(try_data, 'sameword')
-#    print(data)
-
 
